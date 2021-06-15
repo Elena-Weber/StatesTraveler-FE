8e34e1511753c7ca9b9bb1fcaa87204f7fd23a0d
Elena-Weber <crochetmiracles@gmail.com>
2021-06-14 22:20:08 -0400

    work on single sight render

--- js/index.js
+++ js/index.js
@@ -10,7 +10,8 @@ init =()=> {
     getStates(),
     getSights(),
     addArrowsListeners(),
-    createSightForm()
+    createSightForm()//,
+    //getSingleSight()
 }
 
 document.addEventListener("DOMContentLoaded", init)
@@ -152,37 +153,82 @@ sightsColumn.addEventListener("click", event => {
         })
     }
 
-if(event.target.matches(".delete-btn")) {
-    const id = event.target.dataset.id
-    const sightToDelete = document.getElementById(id)
-    fetch(`http://localhost:3000/sights/${id}`, {
-        method: "DELETE",
-        headers: { "Content-Type": "application/json" }
-    })
-    .then(resp => resp.json())
-    .then(sightToDelete.remove())
-}
+    if(event.target.matches(".delete-btn")) {
+        const id = event.target.dataset.id
+        const sightToDelete = document.getElementById(id)
+        fetch(`http://localhost:3000/sights/${id}`, {
+            method: "DELETE",
+            headers: { "Content-Type": "application/json" }
+        })
+        .then(resp => resp.json())
+        .then(sightToDelete.remove())
+    }
 
+    if(event.target.matches(".edit-btn")) {
+        const id = event.target.dataset.id
+        const sightToEdit = document.getElementById(id)
+        console.log(sightToEdit)
+        fetch(`http://localhost:3000/sights/${id}`, {
+            method: "PATCH",
+            headers: { "Content-Type": "application/json" }
+        })
+        .then(resp => resp.json())
+        
+    }
 
 })
 
 createSightForm =()=> {
-const sightForm = document.createElement('form'),
-sightName = document.createElement('input'),
-sightImage = document.createElement('input'),
-sightDetails = document.createElement('input'),
-sightButton = document.createElement('button');
-sightForm.id = 'sight_form',
-sightName.id = 'sight_name',
-sightImage.id = 'sight_image',
-sightDetails.id = 'sight_details',
-sightName.placeholder = 'Sight location',
-sightImage.placeholder = 'Sight image',
-sightDetails.placeholder = 'Sight details',
-sightButton.innerHTML = 'Create sight',
-sightForm.appendChild(sightName),
-sightForm.appendChild(sightImage),
-sightForm.appendChild(sightDetails),
-sightForm.appendChild(sightButton),
-document.getElementById('new_form').appendChild(sightForm)
+    const sightForm = document.createElement('form')
+    sightName = document.createElement('input')
+    sightImage = document.createElement('input')
+    sightDetails = document.createElement('input')
+    sightState = document.createElement('select')
+    sightButton = document.createElement('button')
+    sightForm.id = 'sight_form'
+    sightName.id = 'sight_name'
+    sightImage.id = 'sight_image'
+    sightDetails.id = 'sight_details'
+    sightState.id = 'sight_state'
+    sightButton.id = 'sight_button'
+    sightName.placeholder = 'Name'
+    sightImage.placeholder = 'Image path'
+    sightDetails.placeholder = 'Impressions'
+    //sightState.meaning = State.all
+    sightButton.innerHTML = 'Create a sight'
+    sightForm.appendChild(sightName)
+    sightForm.appendChild(sightImage)
+    sightForm.appendChild(sightDetails)
+    sightForm.appendChild(sightState)
+    sightForm.appendChild(sightButton)
+    document.getElementById('new_form').appendChild(sightForm)
+}
+
+getSingleSight =(sight)=> {
+    const chosenSight = document.querySelector(".sightName")
+    chosenSight.dataset.id = sight.id
+    chosenSight.addEventListener("click", event => {
+    console.log(chosenSight)
+    renderSingleSight()
+    })
+}
+
+renderSingleSight =()=> {
+    const sightDiv = document.createElement('div')
+    sightDiv.classList.add("singleSight")
+    sightDiv.setAttribute("data-id", sight.id)
+    sightDiv.id = sight.id
+    
+    sightDiv.innerHTML = `
+    <h2 data-id="${sight.id}" class="sightName"> ${sight.name} </h2>
+    <img data-id="${sight.id}" src=${sight.image} class="sightPic" />
+    <p data-id="${sight.id}" class="sightDetails">Details: ${sight.details} </p>
+    <p data-id="${sight.id}" class="sightState">State: ${sight.state_id} </p>
+    <p data-id="${sight.id}" class="sightLikes"> ${sight.likes} like(s) </p>
+    <button data-id="${sight.id}" class="like-btn"> Like </button>
+    <button data-id="${sight.id}" class="dislike-btn"> Dislike </button>
+    <button data-id="${sight.id}" class="edit-btn"> Edit </button>
+    <button data-id="${sight.id}" class="delete-btn"> Delete </button>
+    `
+    document.querySelector('single_sight').appendChild(sightDiv)
 }
